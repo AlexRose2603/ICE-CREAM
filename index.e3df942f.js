5eeeaaff4e11d83dc339a8fd90e6bf422912e6b5
AlexRose2603 <111235206+AlexRose2603@users.noreply.github.com>
2023-06-11 21:23:47 +0000

    Deploying to gh-pages from @ AlexRose2603/ICE-CREAM@73fd19b2c9bb2ce0ff9c5efad574b511c840838a 🚀

---
+++ index.e3df942f.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".burger-menu"),s=document.querySelector(".burger-modal-open"),d=document.querySelector(".burger-modal-close-btn");s.addEventListener("click",(()=>{e.classList.remove("is-hidden"),e.classList.add("open")})),d.addEventListener("click",(()=>{e.classList.add("is-hidden"),e.classList.remove("open")}));
+//# sourceMappingURL=index.e3df942f.js.map
