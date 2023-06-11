73fd19b2c9bb2ce0ff9c5efad574b511c840838a
AlexRose2603 <alexandra26korchak@gmail.com>
2023-06-12 00:23:20 +0300

    burger

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
-// const buyButton = document.querySelector('.buy-btn');
-// const closeButton = document.querySelector('.modal-close-btn');
-// const modal = document.querySelector('.modal');
 
-// const openModal = () => {
-//   modal.classList.add('open');
-// };
-
-// const closeModal = () => {
-//   modal.classList.remove('open');
-// };
-
-// buyButton.addEventListener('click', openModal);
-// closeButton.addEventListener('click', closeModal);
---
+++ src/js/burger-menu-modal.js
@@ -0,0 +1,19 @@
+const burgerMenu = document.querySelector('.burger-menu');
+const burgerOpenBtn = document.querySelector('.burger-modal-open');
+const burgerCloseBtn = document.querySelector('.burger-modal-close-btn');
+
+// Function to open burger menu
+const openBurgerMenu = () => {
+  burgerMenu.classList.remove('is-hidden');
+  burgerMenu.classList.add('open');
+};
+
+// Function to close the burger menu
+const closeBurgerMenu = () => {
+  burgerMenu.classList.add('is-hidden');
+  burgerMenu.classList.remove('open');
+};
+
+// Add event listeners to open and close the burger menu
+burgerOpenBtn.addEventListener('click', openBurgerMenu);
+burgerCloseBtn.addEventListener('click', closeBurgerMenu);
