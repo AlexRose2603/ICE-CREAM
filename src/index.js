942e5a3a421641e15da34b71db1b84c2594772f7
AlexRose2603 <alexandra26korchak@gmail.com>
2023-08-12 00:37:11 +0300

    positioning

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 new Swiper('.feedback-slider', {
-  //   navigation: {
-  //     nextEl: '.swiper-button-next',
-  //     prevEl: '.swiper-button-prev',
-  //   },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
