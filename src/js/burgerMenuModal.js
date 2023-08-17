a4aac7566a861faa92bef4d510b99470e2203f01
AlexRose2603 <alexandra26korchak@gmail.com>
2023-08-18 00:39:29 +0300

    done

--- src/js/burgerMenuModal.js
+++ src/js/burgerMenuModal.js
@@ -3,6 +3,11 @@ const burgerOpenBtn = document.querySelector('.burger-modal-open');
 const mobileClose = document.querySelector('.mobile-open-modal');
 const burgerCloseBtn = document.querySelector('.burger-modal-close-btn');
 
+const homeLink = document.getElementById('home');
+const howMadeLink = document.getElementById('how-made');
+const contactsLink = document.getElementById('contacts');
+const productsLink = document.getElementById('products');
+
 // Function to open burger menu
 const openBurgerMenu = () => {
   burgerMenu.classList.remove('is-hidden');
@@ -19,3 +24,8 @@ const closeBurgerMenu = () => {
 burgerOpenBtn.addEventListener('click', openBurgerMenu);
 burgerCloseBtn.addEventListener('click', closeBurgerMenu);
 mobileClose.addEventListener('click', closeBurgerMenu);
+
+homeLink.addEventListener('click', closeBurgerMenu);
+howMadeLink.addEventListener('click', closeBurgerMenu);
+contactsLink.addEventListener('click', closeBurgerMenu);
+productsLink.addEventListener('click', closeBurgerMenu);
