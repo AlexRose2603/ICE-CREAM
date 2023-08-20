3d9c2daad24b07aba9b74ecdd93c649bc91304fd
AlexRose2603 <alexandra26korchak@gmail.com>
2023-08-20 22:52:49 +0300

    all done

--- src/index.js
+++ src/index.js
@@ -4,3 +4,15 @@ new Swiper('.feedback-slider', {
     clickable: true,
   },
 });
+
+/////////For toggling cards in products section
+const cards = document.querySelectorAll('.choice-item');
+
+cards.forEach(card => {
+  const frontSide = card.querySelector('.front-side');
+  const backSide = card.querySelector('.back-side');
+
+  card.addEventListener('click', () => {
+    card.querySelector('.card').classList.toggle('flipped');
+  });
+});
